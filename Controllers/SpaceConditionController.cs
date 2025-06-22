1caf53448b016b149428b60aceeef05a6ca1d007
Erika <erikamc99.com>
2025-06-22 15:03:19 +0200

    refactor(SpaceConditionController): clean up to return DTOs and uniform response

--- Controllers/SpaceConditionController.cs
+++ Controllers/SpaceConditionController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Muuki.Models;
 using Muuki.Data;
+using Muuki.DTOs;
 using MongoDB.Driver;
 
 namespace Muuki.Controllers
@@ -23,11 +23,32 @@ namespace Muuki.Controllers
         {
             if (days <= 0) days = 7;
             var fromDate = DateTime.UtcNow.AddDays(-days);
+
             var conditions = await _context.SpaceConditions
                 .Find(c => c.SpaceId == spaceId && c.Timestamp >= fromDate)
                 .SortByDescending(c => c.Timestamp)
                 .ToListAsync();
-            return Ok(conditions);
+
+            var response = conditions.Select(c => new SpaceConditionEntryDto
+            {
+                Timestamp = c.Timestamp,
+                Humidity = c.Humidity,
+                Temperature = c.Temperature,
+                Pollution = c.Pollution,
+                FoodKg = c.FoodKg,
+                WaterLiters = c.WaterLiters,
+                FoodFrequencyDays = c.FoodFrequencyDays,
+                WaterFrequencyDays = c.WaterFrequencyDays,
+                WellbeingScore = c.WellbeingScore,
+                Alerts = c.Alerts
+            }).ToList();
+
+            return Ok(new
+            {
+                success = true,
+                message = "Histórico de condiciones del espacio",
+                data = response
+            });
         }
     }
 }
